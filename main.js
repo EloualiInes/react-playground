3383b86c87640ac5dacc00ddf8f420c4f09bb5f8
Eloualines <iness240104@gmail.com>
2022-11-14 16:18:42 +0100

    c fait !

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-//const helloWorld = React.createElement('h1', {}, 'Hello world!');
-import React from "react";
-import ReactDOM from "react-dom";
 
-const nameOne = 'Julien JOVY'
-const elementOne = <h1 title={nameOne}>Bonjour</h1>
 
-ReactDOM.render(elementOne, document.querySelector('#app'));
+const getString = (s, uppercaseAll = false) => uppercaseAll ?  s.toUpperCase() : s.slice(0,1).toUpperCase() + s.slice(1).toLowerCase();
+
+const lastName = "el ouali";
+const firstName = "ines";
+const res = (
+    <p>
+        {/* Normalement avec css...*/}
+        <span style={{color:"red"}}>{getString(lastName, true)}</span> <span>{getString(firstName)}</span>
+    </p>
+)
+
+ReactDOM.render(res, document.querySelector('#app'));
+
