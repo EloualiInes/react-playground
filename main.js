fb0e351f5f7b211517f9524c0621b0fe2b7391e9
Eloualines <iness240104@gmail.com>
2022-11-14 21:47:57 +0100

    fini

--- main.js
+++ main.js
@@ -1,63 +1,22 @@
-function Clock(props) {
-    React.useEffect(() => {
-        tick();
-    }, []);
-    
-    const [date, setDate] = React.useState(new Date());
-    const [textColor, setTextColor] = React.useState("black");
-    const intervalRef = React.useRef();
-    const tick = () => {
-        intervalRef.current = setInterval(() => {
-            setDate(new Date());
-        }, 1000)
-        
-    }
+function UserGreeting(props) {
+    return <h1>Bienvenue !</h1>;
+  }
+  
+  function GuestGreeting(props) {
+    return <h1>Veuillez vous inscrire.</h1>;
+  }
+  function Greeting({isLoggedIn}) {
+    const [logIn, setLogIn] = React.useState(isLoggedIn);
 
-    const changeColor = () => {
-        var randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`; 
-        setTextColor(randomColor);
-    }
-
-    const resetColor = () => {
-        setTextColor('black')
-    }
-
-    const stopClock = (e) => {
-        e.preventDefault();
-        clearInterval(intervalRef.current);
-    }
-
-
-    const restartClock = (e) => {
-        e.preventDefault();
-        tick();
-    }
-    
-
-/*     return (
+    return (
         <div>
-            <h1>Hello world</h1>
-            <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
-            <button onClick={changeColor}>Changer couleur</button>
-            <button onClick={resetColor}>Réinitialiser couleur</button>
+           { logIn ? <UserGreeting /> : <GuestGreeting />}
+            <button onClick={ () => {setLogIn(!logIn)}}>Change</button>
         </div>
-        ); */
-
-        // Solution Bonus
-
-        return (
-            <div>
-                <h1>Hello world</h1>
-                <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
-                <button onClick={stopClock}>Stop</button>
-                <button onClick={restartClock}>Reprendre</button>
-            </div>
-            );
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app'));
-
-
-
-
-
+    );
+  }
+  
+  ReactDOM.render(
+    <Greeting isLoggedIn={false} />,
+    document.getElementById('app')
+  );
