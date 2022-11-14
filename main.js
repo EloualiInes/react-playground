b6e4035e118daeb3dda4b7b7ac6829ed3553110c
Eloualines <iness240104@gmail.com>
2022-11-14 15:22:34 +0100

    ok

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const helloWorld = React.createElement('h1', {}, 'Hello world!');
-
-
+// const helloWorld = React.createElement('h1', {}, 'Hello world!');
 // mauvaise pratique
 
-
+console.log("hihi");
 //ReactDOM.render(helloWorld, document.body);
-
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+const elt = <h1>Hello world</h1>;
+console.log(elt);
+ReactDOM.render(
+    elt,
+    document.getElementById('app')
+  );
