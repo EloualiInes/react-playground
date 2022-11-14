bc2926f3cd00b0c23de4a478db37ff54299661ba
Eloualines <iness240104@gmail.com>
2022-11-14 22:43:08 +0100

    fini

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-const NameForm = (props) => {
-    const [value, setValue] = React.useState("");
+const Formulaire = (props) => {
+    const [name, setName] = React.useState("");
+    const [parfum, setParfum] = React.useState("coconut");
+    const [essay, setEssay] = React.useState("");
   
-    const handleChange = (e) => {
-      setValue(e.target.value)
+    const handleChangeName = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleChangeParfum = (e) => {
+        setParfum(e.target.value);
+    }
+
+    const handleChangeEssay = (e) => {
+        setEssay(e.target.value);
     }
   
     const handleSubmit = (e) => {
-      alert(`Le nom a été soumis : ${value}`);
+      alert(`Le nom a été soumis : ${name} \n Le parfum a été soumis : ${parfum} \n Le texte saisi est le suivant : ${essay}`);
       e.preventDefault();
     }
     return (
         <form onSubmit={e => handleSubmit(e)}>
             <label>
                 Nom :
-                <input type="text" value={value} onChange={e => handleChange(e)} />
+                <input type="text" value={name} onChange={e => handleChangeName(e)} />
+            </label>
+            <label>
+                Essay:
+                <textarea value={essay} onChange={e => handleChangeEssay(e)} />
+            </label>
+            <label>
+                Choisissez votre parfum favori :
+                <select value={parfum} onChange={e => handleChangeParfum(e)}>
+                    <option value="grapefruit">Pamplemousse</option>
+                    <option value="lime">Citron vert</option>
+                    <option value="coconut">Noix de coco</option>
+                    <option value="mango">Mangue</option>
+                </select>
             </label>
           <input type="submit" value="Envoyer" />
         </form>
       );
   }
+
+  ReactDOM.render(
+    <Formulaire />,
+    document.querySelector('#app')
+  );
